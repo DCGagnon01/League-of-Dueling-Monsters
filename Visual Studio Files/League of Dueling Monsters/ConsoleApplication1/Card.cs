78aa2fd8892f6f49f33957bd55721d34ed084128
DCGagnon01 <deancgagnon7@gmail.com>
2017-04-12 12:58:27 -0400

    Reorganized Project
    Reorganized the project and fixed minor issues with the .csv file

---
+++ Visual Studio Files/League of Dueling Monsters/ConsoleApplication1/Card.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//using UnityEngine;
+
+
+
+public class Card
+{
+    private string name;
+    private string type;
+    private int atk;
+    private int def;
+    private int summonCost;
+    private int ID;
+
+    public Card(string cardName, string cardType, int cardAtk, int cardDef, int cardCost, int cardID)
+        {
+        name = cardName;
+        type = cardType;
+        atk = cardAtk;
+        def = cardDef;
+        summonCost = cardCost;
+        ID = cardID;
+
+    }
+
+
+
+    public override string ToString()
+    {
+        return name + " a " + type + " card.";
+    }
+
+
+
+
+
+}
+
---
+++ Visual Studio Files/League of Dueling Monsters/ConsoleApplication1/Deck.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//using UnityEngine;
+using System.IO;
+
+public class Deck
+{
+    private Card[] deck;
+    private int currentCard;
+    private const int deckSize = 20;
+    private System.Random randomNum;
+    public int stop;
+
+    public Deck()
+    {
+        //read in ethan database info
+        using (var fs = File.OpenRead(@"C:\League of Dueling Monsters Database.csv"))
+        using (var reader = new StreamReader(fs))
+        {
+            List<string> listID = new List<string>();
+            List<string> listName = new List<string>();
+            List<string> listStars = new List<string>();
+            List<string> listAtk = new List<string>();
+            List<string> listDef = new List<string>();
+
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(',');
+
+                listID.Add(values[0]);
+                listName.Add(values[1]);
+                listStars.Add(values[2]);
+                listAtk.Add(values[3]);
+                listDef.Add(values[4]);
+            }
+
+            Console.WriteLine("{0,-10}{1,-50}{2,-10}{3,-10}{4,-10}", "ID", "Name", "Stars", "Attack", "Defence" + "\n");
+                for (int i=0; i < 100; i++)
+                {
+                    Console.WriteLine("{0,-10}{1,-50}{2,-10}{3,-10}{4,-10}", listID[i], listName[i], listStars[i], listAtk[i], listDef[i]);
+                }
+            Console.ReadLine();
+        }
+        
+
+
+
+
+
+    }
+}
